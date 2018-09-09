2b37213deda940305dd64c192e17aeb3843464f8
kenji.kudou <protoolsmbox727@gmail.com>
2018-09-09 20:27:31 +0900

    Add flush message

--- src/controllers/IndexServlet.java
+++ src/controllers/IndexServlet.java
@@ -42,6 +42,10 @@ public class IndexServlet extends HttpServlet {
         em.close();
 
         request.setAttribute("tasks", tasks);
+        if (request.getSession().getAttribute("flush") != null) {
+            request.setAttribute("flush", request.getSession().getAttribute("flush"));
+            request.getSession().removeAttribute("flush");
+        }
 
         RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/index.jsp");
         rd.forward(request, response);
